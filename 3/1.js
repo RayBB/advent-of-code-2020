b4bd403600934f9f4c5e9f658daa736054b58483
Ray Berger <RayBB@users.noreply.github.com>
2020-12-03 11:43:06 -1000

    day 3

---
+++ 3/1.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const map = fs.readFileSync('input.txt', 'utf8').split("\n")
+
+function countCharactersInPath(down, right, map){
+    let foundChars = {};
+    let xIndex = 0;
+    let yIndex = 0;
+    const xMax = map[0].length - 1;
+    const yMax = map.length - 1;
+
+    while (yIndex <= yMax){
+        const currentChar = map[yIndex][xIndex];
+        //console.log(currentChar)
+        if (foundChars[currentChar] === undefined){
+            foundChars[currentChar] = 1;
+        } else {
+            foundChars[currentChar] += 1;
+        }
+        xIndex = getNextRight(xIndex, xMax, right);
+        yIndex += down;
+    }
+    return foundChars;
+}
+
+function getNextRight(current, max, increment){
+    const sum = current + increment;
+    if (sum <= max) return sum;
+    return sum-max-1;
+}
+
+// get next right tests
+// console.log(getNextRight(3, 3, 1), "0")
+// console.log(getNextRight(3, 4, 1), "4")
+// console.log(getNextRight(3, 4, 2), "0")
+// console.log(getNextRight(9, 10, 3), "1")
+
+console.log(countCharactersInPath(1,3,map))
